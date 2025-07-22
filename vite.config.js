e72fa7c7cc8ea0c727558394b389554c1c3dce9d
Michael Uhlenberg <michael.uhlenberg@t-online.de>
2025-07-22 22:59:10 +0200

    vite MPA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        quiz: resolve(__dirname, "quiz.html"),
+        ende: resolve(__dirname, "ende.html"),
+      },
+    },
+  },
 });
